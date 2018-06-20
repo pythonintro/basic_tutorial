d8a5623151321236843938d6ff36c7a2fdd7b1d8
pythonintro <admin@pythonintro.com>
2018-06-20 13:19:27 +0200

    added info header

--- using_for_loops.py
+++ using_for_loops.py
@@ -1,4 +1,8 @@
-
+#
+# License: GPLv3
+# Source: https://www.pythonintro.com
+# Author: pythonintro.com
+#
 # Using the same lists as we previously created in the using_lists.py example we will now go ahead and print out all items in the lists
 # using for loops!
 
