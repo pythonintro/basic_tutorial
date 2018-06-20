b73a2b0cb494a09c3882391fe95b0d644d388973
pythonintro <admin@pythonintro.com>
2018-06-20 13:28:50 +0200

    initial commit

---
+++ input.py
@@ -0,0 +1,13 @@
+#
+# License: GPLv3
+# Source: https://www.pythonintro.com
+# Author: pythonintro.com
+#
+
+# Using input to fetch user input.
+
+name = input("Enter your name: ")
+print("Hello", name)
+>>> Enter your name
+admin
+>>> "Hello admin"
