0bd0a61c62af74df60be1304680914bc150b562a
pythonintro <admin@pythonintro.com>
2018-06-20 13:11:01 +0200

    added notes

--- using_lists.py
+++ using_lists.py
@@ -1,3 +1,9 @@
+# License: GPLv3
+# Source: https://www.pythonintro.com
+#
+# Author: pythonintro.com
+#
+#
 #Example 1:
 # Create a list of numbers (integers in this case):
 numberslist = [1, 2, 3, 4, 5]
