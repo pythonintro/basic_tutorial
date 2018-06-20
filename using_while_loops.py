1715a7044fba06dfa212579a21e94e5ebe1b7c6d
pythonintro <admin@pythonintro.com>
2018-06-20 13:19:18 +0200

    initial commit

---
+++ using_while_loops.py
@@ -0,0 +1,14 @@
+#
+# License: GPLv3
+# Source: https://www.pythonintro.com
+# Author: pythonintro.com
+#
+
+
+x = 1
+
+while x < 10:
+	# Print information about x:
+	print("x is less than 10", x)
+	# Add 1 to x:
+	x += 1
