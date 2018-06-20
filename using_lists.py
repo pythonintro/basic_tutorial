72bfc6b2f0b1832987c96e0f39aa4f7adcffe61c
pythonintro <admin@pythonintro.com>
2018-06-20 11:23:59 +0200

    Moved loops to using_loops.py

--- using_lists.py
+++ using_lists.py
@@ -1,20 +1,19 @@
 # Create a list of numbers:
 numberslist = [1, 2, 3, 4, 5]
-# Print all items in list:
-for item in list:
-	print(item)
+#Print first item in numberslist: (python counts 0 as the first item.)
+print(numberslist[0])
+>>> 1
+
+
 
 # Create a list of words:
 wordlist = ["hello", "sir", "how are you doing?"]
+print(wordlist[0])
+>>> "hello"
 
-# Print all items in list:
-for word in wordlist:
-	print(word)
+
 	
 # lists can contain several different datatypes:
 my_list = [1,2,3, "Hello Sir!"]
-
-for item in my_list:
-	print(item)
-
-
+print(my_list[0], my_list[3])
+>>> 1 "Hello Sir!"
---
+++ using_loops.py
@@ -0,0 +1,22 @@
+
+# Using the same lists as we previously created in the using_lists.py example we will now go ahead and print out all items in the lists
+# using for loops!
+
+# Create a list of numbers:
+numberslist = [1, 2, 3, 4, 5]
+# Print all items in list:
+for item in list:
+	print(item)
+
+# Create a list of words:
+wordlist = ["hello", "sir", "how are you doing?"]
+
+# Print all items in list:
+for word in wordlist:
+	print(word)
+	
+# lists can contain several different datatypes:
+my_list = [1,2,3, "Hello Sir!"]
+
+for item in my_list:
+	print(item)
